fced17d871d8251eb5597839d7e2d2db54cfd9ac
Alim <alim-halilko@mail.ru>
2020-03-21 01:21:05 +0300

    добавлен блок -факты-

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-// $(document).ready(function() {
-//     $('#select_language').selectric();
-// });
+$(document).ready(function() {
+    $(".ripple_button").click(drawRipple);
+});
 
 $(function() {
     $('#select_language').selectric();
 });
+
+//Ripple Event Handler
+var drawRipple = function(ev) {
+    var x = ev.clientX;
+    var y = ev.clientY;
+    var node = document.querySelector(".ripple");
+    var newNode = node.cloneNode(true);
+    newNode.classList.add("animate");
+    newNode.style.left = ev.clientX - 5 + "px";
+    newNode.style.top = ev.clientY - 5 + "px";
+    node.parentNode.replaceChild(newNode, node);
+};
